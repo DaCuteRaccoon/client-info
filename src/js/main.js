0f3e8f4037f625c515d0288b7878837d6da8753a
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-12-18 12:25:46 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 (function(){ x = document.getElementById("browser"); x.innerText = 'browserNameHere'; delete(x); })();
+
+checkExist = setInterval(function() {
+  if ($('#browser').length) {
+    console.log("Exists!");
+    clearInterval(checkExist);
+    delete(checkExist);
+  } else {
+    // Do this if it doesn't exist
+  }
+}, 100); // check every 100ms
